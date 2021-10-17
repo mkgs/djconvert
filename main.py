1f8c0cd564ec71e39308d5382989a558105709cf
Michael Gisi <gisi@umich.edu>
2021-10-16 23:58:16 -0400

    Update main.py

--- main.py
+++ main.py
@@ -39,24 +39,44 @@ def process_file(in_file, out_file):
         return False
 
     print(f'   converting {in_file}')
+    print(f'      loading...')
 
     sound = AudioSegment.from_file(in_file)
     processed = False
+    sample_rate = str(min(MAX_FRAME_RATE, sound.frame_rate))
     if sound.frame_rate > MAX_FRAME_RATE:
         print(f'      {sound.frame_rate}Hz -> {MAX_FRAME_RATE}Hz')
-        sound.set_frame_rate(MAX_FRAME_RATE)
         processed = True
+    bit_depth = min(MAX_SAMPLE_WIDTH, sound.sample_width) * 8
+    if bit_depth == 8:
+        bit_depth = '-sample_fmt u8'
+    elif bit_depth in (16, 32):
+        bit_depth = f'-sample_fmt s{bit_depth}'
+    elif bit_depth == 24:
+        #bit_depth = '-c:a pcm_s24le'
+        bit_depth = '-sample_fmt s16'
+    else:
+        raise NotImplemented()
     if sound.sample_width > MAX_SAMPLE_WIDTH:
         print(f'      {sound.sample_width * 8}bit -> {MAX_SAMPLE_WIDTH * 8}bit')
-        sound.set_sample_width(MAX_SAMPLE_WIDTH)
         processed = True
     if processed:
-        sound.export(out_file)
+        print(f'      saving...')
         if file_ext in ['.aiff', '.AIFF']:
+            sound.export(out_file, format='aiff', parameters=['-ar', sample_rate, '-sample_fmt', 's16'])
             newdata = music_tag.load_file(out_file)
             for k in metadata.tag_map.keys():
-                if k.startswith('#'):
+                if k.startswith('#') or k == 'artwork':
                     continue
                 newdata[k] = metadata[k].value
+            try:
+                with open(os.path.join(os.path.dirname(in_file), 'cover.jpg'), 'rb') as img_in:
+                    newdata['artwork'] = img_in.read()
+            except Exception as e:
+                raise e
             newdata.save()
+        elif file_ext in ['.wav', '.WAV']:
+            sound.export(out_file, format='wav', parameters=['-ar', sample_rate, '-sample_fmt', 's16'])
+        else:
+            raise NotImplemented()
     return processed
